5d72829cc659918053acec0fc4e51b698f12db8d
jkstone321 <jkstone321@gmail.com>
2022-04-25 21:03:37 -0400

    added function to make html based on user inputs and added validation to the inputs

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-//STARTING OF THE POJECT GOOD LIKE THE LIL POGCHAMP I IS
-//FIRST I NEED TO GET THE PROMPTS SET UP I WAS THINKING OF DOING A WHOLE PROMPTS.JS FOLDER WITH THE DIFFERENT PROMPTS I WILL NEED
-//ILL NEED A PROMPT FOR MANAGER A PROMPT FOR ENGINEER AND A PROMPT FOR INTERN
-//FIGURE OUT SOME WAY TO STORE THOSE VALUES MAYBE HOLD THEM IN AN ARRAY
-//I NEED TO MAKE A MOCK HTML SO I CAN GET STYLING AND WHAT NOT
-//I NEED TO GET THE INFORMATION FROM THE ARRAY ONTO THE HTML
-//ILL ALSO NEED TO CHECK THE ACCEPTANCE CRITERIA AND MAKE SURE I AM USING EVERYTHING THEY WANT
-//....tests.... bleh
+const Prompt = require("./lib/Prompt");;
 
-const Prompt = require("./lib/Prompt");
-const Building = require("./lib/Building")
-const build = new Building();
-//first we need to ask them for manager stuff
-//then we ask for engineer, intern, or stop
-
-//this holds all the information that will be used to build the html later
-
-//these are all of the prompt functions
-
-// const prompt = new Prompt;
-// prompt.getManager();
-
-build.getConsole();
+const prompt = new Prompt;
+prompt.getManager();
